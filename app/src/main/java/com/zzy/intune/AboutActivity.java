9b9ee2a6e2c76ccfc639fe19cf6a98985060516c
zhuzhengyu <3543226907@qq.com>
2025-09-28 23:39:45 +0800

    修改关于页内容

--- app/src/main/java/com/zzy/intune/AboutActivity.java
+++ app/src/main/java/com/zzy/intune/AboutActivity.java
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public final class AboutActivity extends AppCompatActivity {
 
     private static final String GITHUB_URL = "https://github.com/brassface/InTune"; // 固定写死
-    private static final String FEEDBACK_EMAIL = "support@example.com"; // 固定写死
+    private static final String FEEDBACK_EMAIL = "2056882324@qq.com"; // 固定写死
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +26,12 @@ public final class AboutActivity extends AppCompatActivity {
 
         tvVer.setText("v" + version);
 
-        // 更加活泼的文案
         String message = "感谢使用 合调 (InTune)! \n\n" +
-                "如果觉得好用，欢迎到 GitHub 给我们点个 Star ✨\n" +
+                "如果觉得好用，欢迎到 GitHub 给作者点个 Star ✨\n" +
                 "你的支持，是我们持续优化的动力 ( •̀ ω •́ )✧\n\n" +
-                "有任何点子/吐槽/建议，都可以发邮件告诉我们：" + FEEDBACK_EMAIL + "\n" +
-                "记得在标题写上：合调用户反馈 —— 方便我们快速定位（鞠躬）m(_ _)m\n\n" +
-                "本软件为免费开源软件。如果您是通过付费渠道获取，请立即申请退款，并从官方渠道下载。";
+                "有任何点子/吐槽/建议，都可以发邮件告诉作者：" + FEEDBACK_EMAIL + "\n" +
+                "记得在标题写上：合调用户反馈 —— 方便快速定位（鞠躬）m(_ _)m\n\n" +
+                "本软件为免费开源软件。如果您是通过付费渠道获取，请立即申请退款，并从GitHub发布页下载。";
         tvMsg.setText(message);
 
         tvGithub.setOnClickListener(v -> {
